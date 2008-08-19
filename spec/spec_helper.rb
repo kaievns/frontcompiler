3f7537b392ba922c3f7f2166689cfaee98c6aede
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2008-08-19 20:57:51 +0400

    added the init script

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,2 @@
 require "spec"
-require File.dirname(__FILE__)+"/../src/front_compiler"
-require File.dirname(__FILE__)+"/../src/front_compiler/js_compactor"
-require File.dirname(__FILE__)+"/../src/front_compiler/js_compactor/util"
-require File.dirname(__FILE__)+"/../src/front_compiler/js_compactor/names_compactor"
-require File.dirname(__FILE__)+"/../src/front_compiler/js_compactor/shortcuts_converter"
-
+require File.dirname(__FILE__)+"/../init"
