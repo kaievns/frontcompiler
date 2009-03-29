c337b7d9166dc469589ee9cb3d20439e6ee43ae3
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2009-03-29 15:52:18 +0400

    further self-building script implementation

--- lib/front_compiler/java_script/self_builder.rb
+++ lib/front_compiler/java_script/self_builder.rb
@@ -11,7 +11,7 @@ class FrontCompiler
     module SelfBuilder
       def create_self_build
         rehashed_version = process_hashes_in(self)
-        rehashed_version.size > self.size ? self : rehashed_version
+#        rehashed_version.size > self.size ? self : rehashed_version
       end
       
     protected
@@ -20,86 +20,188 @@ class FrontCompiler
         create_build_script *compact_hashes_in(string)
       end
       
-      def create_build_script(source, names_map)
+      def create_build_script(source, maps)
         "eval((function(){"+
           "var s=\"#{source.gsub("\\", "\\\\\\\\").gsub("\n", '\\n').gsub('"', '\"').gsub('\\\'', '\\\\\\\\\'')}\","+
-          "d={#{names_map.collect{ |k, v| "#{k}:\"#{v}\"" }.join(',')}};"+
+          
+          # building the replacements data
+          "d={#{maps.collect{ |k, v| "#{k}:\"#{v}\"" }.join(',')}};"+
+            
+          # building the postprocessing script
           "for(var k in d)"+
-            "s=s.replace(new RegExp('((\\\\{|,)\\\\s*)'+k+'(\\\\s*:)','g'),'$1'+d[k]+'$3')"+
-               ".replace(new RegExp('(\\\\.)'+k+'([^a-zA-Z0-9_\\\\$])','g'),'$1'+d[k]+'$2');"+
+            "s=s.replace(new RegExp('#{REPLACEMENTS_PREFIX}'+k,'g'),d[k]);"+
+          
           "return s"+
         "})());"
       end
       
       def compact_hashes_in(string)
-        string = string.dup
-        
-        names_map = guess_names_map_for(string)
-        
-        names_map.each do |new_name, old_name|
-          string.gsub! /((\{|,)\s*)#{old_name}(\s*:)/ do
-            $1 + new_name + $3
-          end
-          string.gsub! /(\.)#{old_name}([^a-zA-Z0-9_\$])/ do
-            $1 + new_name + $2
-          end
-        end
-        
-        [string, names_map]
+        compress_string(string, [
+          [:guess_names_map_for, :js_hash_key_re, :js_hash_use_re],
+          [:guess_structs_map_for, :js_structs_re],
+          [:guess_commands_map_for, :js_commands_re],
+          [:guess_objects_map_for, :js_objects_re]
+        ])
       end
       
+      #
+      # Guesses the names map to shortify hashes keys in the source code
+      #
       def guess_names_map_for(string)
-        keys = string.scan(/(\{|,)\s*([a-z_\$][a-z0-9_\$]+)\s*:/i
-          ).collect(&:last).collect(&:to_s).concat(js_native_keys(string)).uniq
-        replacements = (1..3).collect{|i| ('a'*i..'z'*i).to_a + ('A'*i..'Z'*i).to_a}.flatten
+        keys = string.scan(/(\{|,)\s*([a-z_\$][a-z0-9_\$]+)\s*:/i).collect(&:last).collect(&:to_s)
+        keys = worth_of_replace(keys+JS_NATIVE_KEYS, string){|name| js_hash_use_re(name)}
         
-        names_map = {}
+        guess_replacements_map(string, keys.uniq) do |name|
+          [js_hash_key_re(name), js_hash_use_re(name)]
+        end
+      end
+      
+      def js_hash_key_re(name)
+        /([\{,]\s*)#{name}(\s*:)/
+      end
+      
+      def js_hash_use_re(name)
+        /(\.)#{name}([^a-zA-Z0-9_\$])/
+      end
+      
+      #
+      # Guesses the replacements map for the standard constructions like
+      #  function (), switch (), while ()
+      #
+      def guess_structs_map_for(string)
+        keys = worth_of_replace(JS_STRUCTS, string){|name| js_structs_re(name)}
+        
+        guess_replacements_map(string, keys) do |name|
+          [js_structs_re(name)]
+        end
+      end
+      
+      def js_structs_re(name)
+        /([^a-zA-Z0-9_\$])#{name}((\s*|(\s+[a-zA-Z0-9_\$]+\s*))\()/
+      end
+      
+      #
+      # guesses the replacements map for the commands like return, throw, catc etc.
+      #
+      def guess_commands_map_for(string)
+        keys = worth_of_replace(JS_COMMANDS, string) {|name| js_commands_re(name)}
+        
+        guess_replacements_map(string, keys) do |name|
+          [js_commands_re(name)]
+        end
+      end
+      
+      def js_commands_re(name)
+        /([^a-zA-Z0-9_\$])#{name}([^a-zA-Z0-9_\$])/
+      end
+      
+      
+      #
+      # guesses the replacements map for the javascript often used objects
+      #
+      def guess_objects_map_for(string)
+        keys = worth_of_replace(JS_OBJECTS, string){ |name| js_objects_re(name) }
+        
+        guess_replacements_map(string, keys) do |name|
+          [js_objects_re(name)]
+        end
+      end
+      
+      def js_objects_re(name)
+        /([^a-zA-Z0-9_\$])#{name}(\.)/
+      end
+      
+      #
+      # checks which of the given keys are used enough to be compressed
+      #
+      def worth_of_replace(keys, string, &block)
+        keys.select do |name|
+          string.scan(yield(name)).size >= MINIMUM_NUMBER_OF_APPEARANCES
+        end
+      end
+      
+      #
+      # Generic replacements quessing method
+      #
+      def guess_replacements_map(string, keys, &block)
+        map = {}
         keys.each do |old_name|
-          new_name = old_name[/[a-z]/i] || 'a'
+          new_name = old_name[/[a-z]{2}/i] || 'aa'
           
-          while names_map.has_key?(new_name) or 
-            string.match(/(\{|,)\s*#{new_name}\s*:/) or
-            string.match(/\.#{new_name}[^a-zA-Z0-9_\$]/)
-            
-            new_name = replacements.shift
+          while map.has_key?(new_name) or string.match(/#{REPLACEMENTS_PREFIX}#{new_name}/)
+            new_name = REPLACEMENTS.shift
             break if new_name.nil? # <- safety break if no possible match found
           end
           
           if new_name and new_name.size < old_name.size
-            names_map[new_name] = old_name
+            map[new_name] = old_name
           end
         end
         
-        names_map
+        map
       end
       
-      def js_native_keys(string)
-        JS_NATIVE_KEYS.select do |key|
-          # taking only the keys which has more than one entry so it was worth of compacting
-          string.scan(/\.#{key}[^a-zA-Z0-9_\$]/).size > 2 
+      REPLACEMENTS = [2].collect{|i| ('a'*i..'z'*i).to_a + ('A'*i..'Z'*i).to_a}.flatten.sort_by(&:size)
+      REPLACEMENTS_PREFIX = '@'
+      
+      #
+      # Handles the source code remaping
+      #
+      def compress_string(string, replacements)
+        string = string.dup
+        maps = {}
+        
+        replacements.each do |options|
+          map = send(options.shift, string)
+          map.each do |new_name, old_name|
+            options.each do |re_method|
+              string.gsub! send(re_method, old_name) do
+                $1 + REPLACEMENTS_PREFIX + new_name + $2
+              end
+            end
+          end
+          
+          maps.merge!(map)
         end
+        
+        [string, maps]
       end
       
     public
+      
+      MINIMUM_NUMBER_OF_APPEARANCES = 2
+      
       JS_NATIVE_KEYS = %w(
-        prototype apply call userAgent toString toSource
-        push pop shift unshift length indexOf lastIndexOf select split
+        prototype constructor apply call userAgent toString toSource
+        push pop shift unshift length indexOf lastIndexOf select split slice
         setTimeout setInterval clearTimeout clearInterval
         floor ceil round random
-        test match replace toLowerCase toUpperCase substr substring
-        parentNode nodeType tagName
-        firstChild lastChild childNodes
-        nextSibling previousSibling
-        appendChild insertBefore replaceChild removeChild
+        test match replace toLowerCase toUpperCase substr substring charAt charCodeAt fromCharCode
+        parentNode firstChild lastChild childNodes nextSibling previousSibling
+        nodeType nodeValue tagName appendChild insertBefore replaceChild removeChild
         createElement createTextNode createDocumentFragment 
         getElementById getElementsByTagName
         document body parent opener window
         attachEvent addEventListener
         detachEvent removeEventListener
         getAttribute setAttribute removeAttribute
+        offsetHeight offsetWidth offsetTop
         checked disabled selected
-        id name type title value innerHTML
+        name type title value innerHTML responseText responseXML
         style display className defaultView
+      ).freeze
+      
+      JS_STRUCTS = %w(
+        function switch while
+      ).freeze
+      
+      JS_COMMANDS = %w(
+        return break continue default true false catch finally throw arguments case instanceof typeof
+        encodeURIComponent decodeURIComponent escape unescape null
+      ).freeze
+      
+      JS_OBJECTS = %w(
+        document window this self navigator Object String Array Date RegExp Element Prototype
       ).freeze
     end
   end 
--- spec/lib/front_compiler/java_script/self_builder_spec.rb
+++ spec/lib/front_compiler/java_script/self_builder_spec.rb
@@ -2,6 +2,7 @@ require File.dirname(__FILE__)+"/../../../spec_helper"
 
 describe FrontCompiler::JavaScript::SelfBuilder do
   def compact(src)
+#    FrontCompiler::JavaScript::SelfBuilder::MINIMUM_NUMBER_OF_APPEARANCES = 2
     FrontCompiler::JavaScript.new(src).instance_eval do
       compact_hashes_in(self).first
     end
@@ -33,6 +34,7 @@ describe FrontCompiler::JavaScript::SelfBuilder do
               third : 3,
               copy: function() {
                 var a = hash.first.second.third.copy();
+                var a = hash.first.second.third.copy();
                 var b = hash.firstsecond.thirdcopy();
               }
             }
@@ -41,23 +43,24 @@ describe FrontCompiler::JavaScript::SelfBuilder do
       }
     }).should == %{
       var hash = {
-        f:  1,
-        s: 2,
-        t:  3,
-        c: {
-          f:  1,
-          s: 2,
-          t:  3,
-          c: {
-            f : 1,
-            s: 2,
-            t : 3,
-            c: {
-              f : 1,
-              s: 2,
-              t : 3,
-              c: function() {
-                var a = hash.f.s.t.c();
+        @fi:  1,
+        @se: 2,
+        @th:  3,
+        @co: {
+          @fi:  1,
+          @se: 2,
+          @th:  3,
+          @co: {
+            @fi : 1,
+            @se: 2,
+            @th : 3,
+            @co: {
+              @fi : 1,
+              @se: 2,
+              @th : 3,
+              @co: function() {
+                var a = hash.@fi.@se.@th.@co();
+                var a = hash.@fi.@se.@th.@co();
                 var b = hash.firstsecond.thirdcopy();
               }
             }
@@ -77,6 +80,7 @@ describe FrontCompiler::JavaScript::SelfBuilder do
           bear: {},
           communism: function() {
             hash.a.b.c.avrora().bear.communism();
+            hash.a.b.c.avrora().bear.communism();
           }
         }
       }
@@ -85,10 +89,11 @@ describe FrontCompiler::JavaScript::SelfBuilder do
         a: 1,
         b: 2,
         c: {
-          d: {},
-          e: {},
-          f: function() {
-            hash.a.b.c.d().e.f();
+          @av: {},
+          @be: {},
+          @co: function() {
+            hash.a.b.c.@av().@be.@co();
+            hash.a.b.c.@av().@be.@co();
           }
         }
       }
@@ -103,15 +108,17 @@ describe FrontCompiler::JavaScript::SelfBuilder do
         third : function() {
           var hash = hash.f.s.t();
           var hash = hash.first.second.third();
+          var hash = hash.first.second.third();
         }
       }
     }).should == %{
       var hash = {
-        a : 1,
-        b: 2,
-        c : function() {
+        @fi : 1,
+        @se: 2,
+        @th : function() {
           var hash = hash.f.s.t();
-          var hash = hash.a.b.c();
+          var hash = hash.@fi.@se.@th();
+          var hash = hash.@fi.@se.@th();
         }
       }
     }
@@ -124,43 +131,130 @@ describe FrontCompiler::JavaScript::SelfBuilder do
         cAmel: 2,
         caMeL: function() {
           var camel = hash.camel.cAmel().caMeL;
+          var camel = hash.camel.cAmel().caMeL;
         }
       }
     }).should == %{
       var hash = {
-        c: 1,
-        a: 2,
-        b: function() {
-          var camel = hash.c.a().b;
+        @ca: 1,
+        @cA: 2,
+        @aa: function() {
+          var camel = hash.@ca.@cA().@aa;
+          var camel = hash.@ca.@cA().@aa;
         }
       }
     }
   end
   
-  it "should not replace native keys if they present less then three times" do
+  it "should not replace native keys if they present less then twice" do
+    
     FrontCompiler::JavaScript::SelfBuilder::JS_NATIVE_KEYS.each do |key|
       compact(%{
         var something = element.#{key};
-        var another   = something.#{key};
       }).should ==   %{
         var something = element.#{key};
-        var another   = something.#{key};
       }
     end
   end
   
-  it "should replace native keys if they present in the script more than twice" do
+  it "should replace native keys if they present in the script more than once" do
     FrontCompiler::JavaScript::SelfBuilder::JS_NATIVE_KEYS.each do |key|
       compact(%{
         var something = element.#{key};
         var another   = something.#{key};
-        var more      = another.#{key};
       }).should ==   %{
-        var something = element.#{key.slice(0,1)};
-        var another   = something.#{key.slice(0,1)};
-        var more      = another.#{key.slice(0,1)};
+        var something = element.@#{key.slice(0,2)};
+        var another   = something.@#{key.slice(0,2)};
       }
     end
+  end
+  
+  it "should not touch the standard constructions if they are present less then twice" do
+    compact(%{
+      var f = function () {
+        while () {
+          switch () {
+          }
+        }
+      }
+    }).should == %{
+      var f = function () {
+        while () {
+          switch () {
+          }
+        }
+      }
+    }
+  end
+  
+  it "should shortify standard constructions when they appear in the code more than once" do
+    compact(%{
+      var function = function() {
+        switch () {
+          while () {
+            do();
+          }
+        }
+      };
+      var function = function name() {
+        switch () {
+          while () {
+            do();
+          }
+        }
+      };
+    }).should == %{
+      var function = @fu() {
+        @sw () {
+          @wh () {
+            do();
+          }
+        }
+      };
+      var function = @fu name() {
+        @sw () {
+          @wh () {
+            do();
+          }
+        }
+      };
+    }
+  end
+  
+  it "should not touch javascript commands which appears less than twice" do
+    compact(%{
+      return bla;
+    }).should == %{
+      return bla;
+    }
+  end
+  
+  it "should compress commands which are appears in the code more than once" do
+    compact(%{
+      return bla;
+      return bla;
+    }).should == %{
+      @re bla;
+      @re bla;
+    }
+  end
+  
+  it "should not touch javascript objects which appear less than twice in the code" do
+    compact(%{
+      Object.bla;
+    }).should == %{
+      Object.bla;
+    }
+  end
+  
+  it "should compress objects which apperas in the code more than once" do
+    compact(%{
+      Object.bla;
+      Object.bla;
+    }).should == %{
+      @Ob.bla;
+      @Ob.bla;
+    }
   end
   
   it "should create a correct rebuild script" do
@@ -173,6 +267,6 @@ describe FrontCompiler::JavaScript::SelfBuilder do
           hash.first.second().third
         }
       }
-    }).should == %[eval((function(){var s="\\n      var hash = {\\n        f : '1',\\n        s: \\\"2\\\",\\n        t : /3/,\\n        c: function() {\\n          hash.f.s().t\\n        }\\n      }\\n    ",d={c:"common",f:"first",s:"second",t:"third"};for(var k in d)s=s.replace(new RegExp('((\\\\{|,)\\\\s*)'+k+'(\\\\s*:)','g'),'$1'+d[k]+'$3').replace(new RegExp('(\\\\.)'+k+'([^a-zA-Z0-9_\\\\$])','g'),'$1'+d[k]+'$2');return s})());]
+    }).should == "eval((function(){var s=\"\\n      var hash = {\\n        first : '1',\\n        second: \\\"2\\\",\\n        third : /3/,\\n        common: function() {\\n          hash.first.second().third\\n        }\\n      }\\n    \",d={};for(var k in d)s=s.replace(new RegExp('@'+k,'g'),d[k]);return s})());"
   end
 end
